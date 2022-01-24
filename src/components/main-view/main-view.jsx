eeb9b00c16291b1b0ecc54aefc408d6b6138031b
lwor1987 <60717817+lwor1987@users.noreply.github.com>
2022-01-23 19:56:50 -0500

    Made changes to errors.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -7,9 +7,9 @@ export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies:[ {_id: 1, Title:'Iron Man 3', Description: " When tony Stark''s world is torn apart by formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution.", ImagePath: 'https://th.bing.com/th/id/R.5d1e5f8259313d7b3b83973d92632e59?rik=%2byeCQwRqvUq0%2bQ&pid=ImgRaw&r=0', Genre: 'Action', Director: 'Shane Black'},
-              {_id: 2, Title: 'Black Widow', Description: 'Natasha Romanoff cnfronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises.', ImagePath: 'https://th.bing.com/th/id/OIP.QvgVT6sUu51B1GDPyY-rpwHaKO?pid=ImgDet&rs=1', Genre: 'Adventure', Director: 'Cate Shortland' },
-              {_id: 3, Title: 'Guardians of the Galaxy', Description: 'A group of intergalactic criminals must pull together to stop a fantical warrior with plans to purge the universe.', ImagePath: 'https://th.bing.com/th/id/R.e89d450ebca76f1f0f1064d225bc1ca7?rik=jmGkuvO5ixORww&pid=ImgRaw&r=0', Genre: 'Comedy', Director: 'James Gunn'}
+      movies:[ {_id: 1, Title:'Iron Man 3', Description: " When tony Stark''s world is torn apart by formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution.", ImagePath: 'https://4.bp.blogspot.com/-kJjr_c1_GJI/VTDc8wrDDgI/AAAAAAAACV4/x0-GAv2P5GQ/s1600/ironman3-logo.png', Genre: 'Action', Director: 'Shane Black'},
+              {_id: 2, Title: 'Black Widow', Description: 'Natasha Romanoff cnfronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises.', ImagePath: 'https://th.bing.com/th/id/R.9a7957b79db60ce42ad443f6466d80b3?rik=vtIMR2eNErZwOg&pid=ImgRaw&r=0', Genre: 'Adventure', Director: 'Cate Shortland' },
+              {_id: 3, Title: 'Guardians of the Galaxy', Description: 'A group of intergalactic criminals must pull together to stop a fantical warrior with plans to purge the universe.', ImagePath: 'https://orig00.deviantart.net/6b34/f/2014/178/e/b/guardians_of_the_galaxy_folder_icon_by_87ashish-d7o7u7o.png', Genre: 'Comedy', Director: 'James Gunn'}
     ],
       selectedMovie: null
     };
@@ -24,14 +24,13 @@ export class MainView extends React.Component {
   render() {
     const { movies, selectedMovie } = this.state;
     
-    if (selectedMovie) return <MovieView movie ={selectedMovie} />;
     
     if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
     return (
       <div className="main-view">
        {selectedMovie
-       ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie =>
-      {this,setSelectedMovie(newSelectedMovie); }}/>
+       ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+      
       : movies.map(movie => (
         <MovieCard key={movie._id} movie={movie} onMovieClick
         ={ (movie) => {this.setSelectedMovie(movie) }}/>
