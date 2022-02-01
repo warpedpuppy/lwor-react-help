c71452ed3481db53a2a6b5e56da1f8ed2a689806
lwor1987 <60717817+lwor1987@users.noreply.github.com>
2022-01-31 19:27:14 -0500

    Made changes.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import axios from 'axios';
+<<<<<<< Updated upstream
+=======
+import './main-view.scss';
+
+import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from '../registration-view/registration-view';
+>>>>>>> Stashed changes
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -34,6 +41,7 @@ export class MainView extends React.Component {
   render() {
     const { movies, selectedMovie } = this.state;
     
+<<<<<<< Updated upstream
     
     if (movies.length === 0) return <div className="main-view" />;
     return (
@@ -49,9 +57,30 @@ export class MainView extends React.Component {
 
 
       }
+=======
+    //if(!user) return <LoginView onLoggedIn= {users => this.onLoggedIn(user)} />;
+
+    if (movies.length === 0) return <div className="main-view" / >;
+   
+   return (
+  <Row className="main-view justify-content-md-center">
+    {selectedMovie
+      ? (
+        <Col md={8}>
+          <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+        </Col>
+      )
+      : movies.map(movie => (
+        <Col md={3}>
+          <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+        </Col>
+      ))
+    }
+  </Row>
+);
+    }
+>>>>>>> Stashed changes
        
-      </div>
-    );
-  }
+    
 }
 
