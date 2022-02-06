d913c4f6794d4d0fa74f3e72a11107ef9c682df8
lwor1987 <60717817+lwor1987@users.noreply.github.com>
2022-02-05 22:06:09 -0500

    Added in some lines.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import axios from 'axios';
 
-import './main-view.scss';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 
 import { LoginView } from '../login-view/login-view';
 import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import './main-view.scss';
 export class MainView extends React.Component {
   
   constructor() {
     super();
     this.state = {
       movies:[],
-      selectedMovie: null
+      user: null
     };
   }
 
@@ -57,49 +62,39 @@ export class MainView extends React.Component {
 
 
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, user } = this.state;
     
-
+    if (!user) return <Row>
+    <Col>
+      <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+    </Col>
+  </Row>
     
     if (movies.length === 0) return <div className="main-view" />;
     return (
-      <div className="main-view">
-       {selectedMovie
-       ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-      
-      : movies.map(movie => (
-        <MovieCard key={movie._id} movie={movie} onMovieClick
-        ={ (movie) => {this.setSelectedMovie(movie) }}/>
+      <Router>
+        <Row className="main-view justify-content-md-center">
+          <Route exact path="/" render={() => {
+            return movies.map(m => (
+              <Col md={3} key={m._id}>
+                <MovieCard movie={m} />
+              </Col>
+            ))
+          }} />
+          <Route path="/movies/:movieId" render={({ match }) => {
+            return <Col md={8}>
+              <MovieView movie={movies.find(m => m._id === match.params.movieId)} />
+            </Col>
+          }} />
 
-      ))
-
-
-      }
-
-
-
-    if(!user) return <LoginView onLoggedIn= {users => this.onLoggedIn(user)} />;
-
-    if (movies.length === 0) return <div className="main-view" / >;
-   
-   return (
-  <Row className="main-view justify-content-md-center">
-    {selectedMovie
-      ? (
-        <Col md={8}>
-          <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-        </Col>
-      )
-      : movies.map(movie => (
-        <Col md={3}>
-          <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-        </Col>
-      ))
-    }
-  </Row>
-
-  </div>
-        );
-    }
-
+        </Row>
+      </Router>
+    );
+  }
 }
+
+
+
+ 
+   
+  
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './movie-card.scss';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 
 export class MovieCard extends React.Component {
   
   render() {
-    const { movie, onMovieClick } = this.props;
-    return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
+    const { movie } = this.props;
+
+    return (
+      <Card>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          <Link to={`/movies/${movie._id}`}>
+            <Button variant="link">Open</Button>
+          </Link>
+        </Card.Body>
+      </Card>
+    );
   }
 }
