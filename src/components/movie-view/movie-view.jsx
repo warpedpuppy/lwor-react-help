42c3a839881913221aee347b0a4624a0f90ee9dc
lwor1987 <60717817+lwor1987@users.noreply.github.com>
2022-08-27 10:51:31 -0400

    Fixed error.

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { Link } from "react-router-dom";
-import axios from 'axios';
+
+import { Card, Col, Container, Row, Button } from "react-bootstrap";
 
 import './movie-view.scss';
 
