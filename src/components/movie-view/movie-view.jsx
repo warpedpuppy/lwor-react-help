d05af58741915427662981bf13ed6452cf1c80b2
lwor1987 <60717817+lwor1987@users.noreply.github.com>
2022-08-27 10:56:29 -0400

    Fixed errors.

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -49,22 +49,4 @@ export class MovieView extends React.Component {
 }
 }
 
-MovieView.propTypes = {
-	movie: PropTypes.shape({
-	  Title: PropTypes.string.isRequired,
-	  Description: PropTypes.string.isRequired,
-	  Genre: PropTypes.shape({
-		Name: PropTypes.string.isRequired,
-		Description: PropTypes.string.isRequired
-	  }),
-	  Director :PropTypes.shape({
-		Name:PropTypes.string.isRequired,
-		Bio:PropTypes.string.isRequired,
-		Birth:PropTypes.string.isRequired,
-		Death:PropTypes.string
-	  }),
-	  ImageURL: PropTypes.string,
-	  Featured: PropTypes.bool
-	}).isRequired,
-	onBackClick: PropTypes.func.isRequired
-  };
+
